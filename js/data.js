12de259fa98fb80d7ab62eed60254999c89c7574
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-19 18:56:33 +0000

    fixed beforeunload function by moving to data.js file, fixed load from local storage as data is saved by user

--- js/data.js
+++ js/data.js
@@ -6,3 +6,12 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntriesJSON = localStorage.getItem('journal-entries-storage');
+data = JSON.parse(previousEntriesJSON);
+
+// this event stores the object models into local storage
+window.addEventListener('beforeunload', event => {
+  var journalArrayJSON = JSON.stringify(data);
+  localStorage.setItem('journal-entries-storage', journalArrayJSON);
+});
--- js/main.js
+++ js/main.js
@@ -8,26 +8,17 @@ $inputUrl.addEventListener('input', function (event) {
 });
 
 var $entryForm = document.getElementById('entry-form');
-var journalArray = [];
-var journalEntry = {};
-var nextEntryId = 1;
 
 // this event captures the input values and adds them into an object
 $entryForm.addEventListener('submit', event => {
   event.preventDefault();
+  var journalEntry = {};
   journalEntry.title = $entryForm.elements.title.value;
   journalEntry.imgUrl = $entryForm.elements.url.value;
   journalEntry.notes = $entryForm.elements.notes.value;
-  journalEntry.entryId = nextEntryId;
-  nextEntryId++;
-  journalArray.unshift(journalEntry);
-  journalEntry = {};
+  journalEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(journalEntry);
   $imgPlaceholder.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
-});
-
-// this event stores the object models into local storage
-window.addEventListener('beforeunload', event => {
-  var journalArrayJSON = JSON.stringify(journalArray);
-  localStorage.setItem('journal-entries-storage', journalArrayJSON);
 });
