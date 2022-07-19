2be246df41ec68fbdc886796d2da49775b1b4439
Adrian Arias <adrianarias.pro@gmail.com>
2022-07-19 15:46:04 +0000

    completed issue-1 create an entry.

--- js/main.js
+++ js/main.js
@@ -8,13 +8,26 @@ $inputUrl.addEventListener('input', function (event) {
 });
 
 var $entryForm = document.getElementById('entry-form');
-
+var journalArray = [];
 var journalEntry = {};
+var nextEntryId = 1;
 
-// this function adds input values into object (in progress)
+// this event captures the input values and adds them into an object
 $entryForm.addEventListener('submit', event => {
   event.preventDefault();
   journalEntry.title = $entryForm.elements.title.value;
   journalEntry.imgUrl = $entryForm.elements.url.value;
   journalEntry.notes = $entryForm.elements.notes.value;
+  journalEntry.entryId = nextEntryId;
+  nextEntryId++;
+  journalArray.unshift(journalEntry);
+  journalEntry = {};
+  $imgPlaceholder.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $entryForm.reset();
+});
+
+// this event stores the object models into local storage
+window.addEventListener('beforeunload', event => {
+  var journalArrayJSON = JSON.stringify(journalArray);
+  localStorage.setItem('journal-entries-storage', journalArrayJSON);
 });
